db7c278d9bbd5d589b235615681789cdcf0a17d5
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-04-11 16:35:25 +0200

    Add: Milestone 0

--- js/script.js
+++ js/script.js
@@ -5,3 +5,34 @@
 // Lo usiamo come controllo alla fine se c'è qualche problema di sintassi! 
 //"use strict";
 //**************************************************** */
+
+// MILESTONE 0
+const team = [
+    {
+      name: "Wayne Barnett",
+      job: "Founder & CEO" ,
+    },
+    {
+      name: "Angela Caroll",
+      job: "Chief Editor" ,
+    },
+    {
+      name: "Walter Gordon",
+      job: "Office Manager" ,
+    },
+    {
+        name: "Angela Lopez",
+        job: "Social Media Manager " ,
+      },
+      {
+        name: "Scott Estrada",
+        job: "Developer" ,
+      },
+      {
+        name: "Barbara Ramos",
+        job: "Graphic Designer" ,
+      },
+  ];
+  
+  console.log(team[0,1]);
+  
